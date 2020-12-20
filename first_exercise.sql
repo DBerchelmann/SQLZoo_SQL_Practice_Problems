efb5f4043d13ea7b0643ba1b75962f22e00b4890
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-20 13:37:45 -0600

    added additonal completed excercises

--- first_exercise.sql
+++ first_exercise.sql
@@ -95,7 +95,19 @@ FROM world
 WHERE LEFT(name,1 ) = LEFT(capital, 1)
 AND NOT name = capital
 
+# Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They dont count because they have more than one word in the name.
 
+# Find the country that has all the vowels and no spaces in its name. 
+
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %';
+  
 
 
 
