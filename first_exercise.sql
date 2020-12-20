84fda0b1c501787be03f17fee797bff5b17e44ef
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-20 09:17:51 -0600

    added more completed excercises

--- first_exercise.sql
+++ first_exercise.sql
@@ -35,3 +35,30 @@ WHERE population >= 200000000
 SELECT name, population/1000000
 FROM world
 WHERE continent = ('South America')
+
+# Show the name and population for France, Germany, Italy
+
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy')
+
+# Show the countries which have a name that includes the word 'United'
+
+SELECT name
+FROM world
+WHERE name LIKE 'United%'
+
+# Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+
+# Show the countries that are big by area or big by population. Show name, population and area.
+
+SELECT name, population, area
+FROM world
+WHERE  area > 3000000 or
+population > 250000000
+
+# Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 XOR population > 250000000
