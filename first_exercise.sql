03d0f2bbba0acfefeb1f7407ca7047cabd4329a0
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-19 23:32:12 -0600

    added additonal exercise

--- first_exercise.sql
+++ first_exercise.sql
@@ -17,3 +17,21 @@ SELECT name, area
 FROM world
 WHERE area
 BETWEEN 200000 and 250000;
+
+# How to use WHERE to filter records. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+
+SELECT name
+FROM world
+WHERE population >= 200000000
+
+# Give the name and the per capita GDP for those countries with a population of at least 200 million.
+
+SELECT name, gdp/population
+FROM world
+WHERE population >= 200000000
+
+# Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = ('South America')
