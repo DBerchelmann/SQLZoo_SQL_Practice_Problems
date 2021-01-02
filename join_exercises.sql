af3eba0893964f9a7fbd962182b9c442d8722800
David Berchelmann <david.a.berchelmann@gmail.com>
2021-01-01 23:38:27 -0600

    added final join excercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -188,3 +188,21 @@ FROM actor JOIN casting ON (actor.id=casting.actorid)
 WHERE ord = 1
 GROUP BY NAME
 HAVING COUNT(name) >= 15;
+
+# List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+
+SELECT title, COUNT(actorid)
+FROM movie JOIN casting ON (movie.id = casting.movieid)
+WHERE yr = 1978
+GROUP BY title
+ORDER BY COUNT(actorid) DESC, title;
+
+# List all the people who have worked with 'Art Garfunkel'.
+
+SELECT name
+FROM casting JOIN actor ON (casting.actorid = actor.id)
+WHERE movieid IN (
+                  SELECT movieid 
+                  FROM casting JOIN actor ON (casting.actorid=actor.id)
+                  WHERE name = 'Art Garfunkel')
+AND name != 'Art Garfunkel';
